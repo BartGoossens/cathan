710ef2072b494856f65d23d4a5f4383da4a53a62
Bart Goossens <bart@goossens.info>
2017-12-20 22:09:19 +0100

    Included ratpack served webpage containing sample grocery list

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,32 @@
 package hello;
 
+import ratpack.server.BaseDir;
+import ratpack.server.RatpackServer;
+import ratpack.groovy.template.TextTemplateModule;
+import ratpack.guice.Guice;
+
+import static ratpack.groovy.Groovy.groovyTemplate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        RatpackServer.start(s -> s
+                .serverConfig(c -> c
+                        .baseDir(BaseDir.find())
+                        .port(80)
+                        .env())
+                .registry(Guice.registry(b -> b
+                        .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+
+                .handlers(chain -> chain
+                        .get(ctx -> ctx.render(groovyTemplate("index.html")))
+                        .files(f -> f.dir("public"))
+                )
+        );
         SpringApplication.run(Application.class, args);
     }
 }
